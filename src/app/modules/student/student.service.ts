601e65f844867c0a43a30be6a62d93f20ba19db5
Th3At0nic <islammdrahatul@gmail.com>
2024-12-17 22:21:04 +0600

    successfully populated the referred document in student

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,12 +1,26 @@
 import { StudentModel } from './student.model';
 
 const getAllStudentsFromDB = async () => {
-  const result = await StudentModel.find(); //built-in static methods of mongoose
+  const result = await StudentModel.find()
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    }); //built-in static methods of mongoose
   return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-  const result = await StudentModel.findOne({ id }); //built-in static methods of mongoose
+  const result = await StudentModel.findOne({ id })
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    }); //built-in static methods of mongoose
 
   // const result = await StudentModel.aggregate([{ $match: { id: id } }]);
 
