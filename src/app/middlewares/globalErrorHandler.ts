5666e22cc065c73dc68648bb1bcbee3dcfe6aa85
Th3At0nic <islammdrahatul@gmail.com>
2024-12-22 00:14:22 +0600

    added zod validation handler to send structed and controlled error  data

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,24 +1,28 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
 
 import { ErrorRequestHandler } from 'express';
 import { NotFoundError } from '../utils/errors/notFoundError';
 import { ValidationError } from '../utils/errors/validationError';
-import sendResponse from '../utils/sendResponse';
 import { ConflictError } from '../utils/errors/conflictError';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
-export type TErrorSource = {
-  path: string | number;
-  message: string;
-}[];
-
-const errorSource: TErrorSource = [
-  {
-    path: '',
-    message: 'Something went wrong',
-  },
-];
+const handleZodError = (err: ZodError) => {
+  const errorSource: TErrorSource = err.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue.path[issue.path.length - 1],
+      message: issue.message,
+    };
+  });
+  const statusCode = 400;
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorSource,
+  };
+};
 
 export const globalErrorHandler: ErrorRequestHandler = (
   err,
@@ -26,41 +30,42 @@ export const globalErrorHandler: ErrorRequestHandler = (
   res,
   next,
 ) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
   if (err instanceof NotFoundError) {
-    return sendResponse(
-      res,
-      err.statusCode,
-      false,
-      err.message,
-      err.errorSource,
-    );
-    // res.status(err.statusCode).json({ message: err.message });
+    statusCode = err.statusCode;
+    message = err.message;
   }
 
   if (err instanceof ValidationError) {
-    return sendResponse(
-      res,
-      err.statusCode,
-      false,
-      err.message,
-      err.errorSource,
-    );
-    // res.status(err.statusCode).json({ message: err.message });
+    statusCode = err.statusCode;
+    message = err.message;
   }
 
   if (err instanceof ConflictError) {
-    return sendResponse(
-      res,
-      err.statusCode,
-      false,
-      err.message,
-      err.errorSource,
-    );
+    statusCode = err.statusCode;
+    message = err.message;
   }
 
-  res.status(500).json({
+  if (err instanceof ZodError) {
+    const simplifiedZodError = handleZodError(err);
+    statusCode = simplifiedZodError.statusCode;
+    message = simplifiedZodError.message;
+    errorSource = simplifiedZodError.errorSource;
+  }
+
+  res.status(statusCode).json({
     success: false,
-    message: 'Something went wrong!',
+    message,
     errorSource,
+    stack: config.NODE_ENV === 'development' ? err.stack : null,
   });
 };
