6740609df75ec0b46d7522255bce389cbd7447bb
Th3At0nic <islammdrahatul@gmail.com>
2025-01-11 23:09:18 +0600

    done: new interface, statics in user for password check, auth module, implemented related logics, check ifPassValid

---
+++ src/app/utils/errors/UnauthorizedError.ts
@@ -0,0 +1,8 @@
+import { TErrorSource } from '../../interface/error';
+import { AppError } from './AppError';
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string, errorSource: TErrorSource) {
+    super(message, 401, errorSource);
+  }
+}
