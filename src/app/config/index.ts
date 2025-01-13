d4a534cec25129127a882d9c79063f0174bbc11e
Th3At0nic <islammdrahatul@gmail.com>
2025-01-13 07:48:28 +0600

    done: auth middleware, token, implemented in getAllAcademicFaculties

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,4 +9,5 @@ export default {
   database_url: process.env.DATABASE_URL,
   bcrypt_round_salt: process.env.BCRYPT_SALT_ROUND,
   default_password: process.env.DEFAULT_PASSWORD,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
 };
