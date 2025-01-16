31f5b6d3bab7e97a98fc70cf71d54908ad0e0413
Th3At0nic <islammdrahatul@gmail.com>
2025-01-16 11:35:02 +0600

    done: generateToken fun, utils,generate refreshToken, save it to cookie

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -10,4 +10,7 @@ export default {
   bcrypt_round_salt: process.env.BCRYPT_SALT_ROUND,
   default_password: process.env.DEFAULT_PASSWORD,
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
 };
